74ff61614a33faf0aa0379009a39a5a533064f05
Raghav Sharma <redclaww02@gmail.com>
2023-09-30 22:46:59 +0530

    Footer added

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import "../css/Home.css";
-// import game from "../assets/photos/Home/group1757.png";
+import Footer from "../components/Footer";
+import game from "../assets/photos/Home/group1757.png";
 import ecom from "../assets/photos/Home/group1758.png";
 import art from "../assets/photos/Home/group1759.png";
 import luxury from "../assets/photos/Home/group1761.png";
@@ -63,7 +64,7 @@ const Home = () => {
           <div className="box">
             <div className="box-div">
               <div className="box-inner">
-                <img src="" alt="" />
+                <img src={game} alt="" />
                 <h3>Gaming & Sports</h3>
               </div>
               <div className="box-inner">
@@ -168,7 +169,9 @@ const Home = () => {
             </div>
           </div>
       </div>
+      <Footer />
     </div>
+    
   );
 };
 
